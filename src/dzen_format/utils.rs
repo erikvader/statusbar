dd3a666e76352d04c9bbc6e3d25aea938882719c
Erik Rimskog <erik.rimskog@gmail.com>
2020-04-03 22:59:16 +0200

    color_step

--- src/dzen_format/utils.rs
+++ src/dzen_format/utils.rs
@@ -5,4 +5,28 @@ impl<'a> DzenBuilder<'a> {
     pub fn name_click(self, button: &'a str, module_name: &'a str) -> Self {
         self.click(button, &["echo ", module_name, " click ", button, " >> ", FIFO_PATH])
     }
+
+    pub fn color_step(self, steps: &[(i32, &'a str)]) -> Self {
+        let inner: i32 = if let Ok(n) = self.to_string().parse() {
+            n
+        } else {
+            eprintln!("couldn't parse as number");
+            return self;
+        };
+
+        let mut color = None;
+        for (lim, col) in steps.iter() {
+            if inner >= *lim {
+                color = Some(col);
+            } else {
+                break;
+            }
+        }
+
+        if let Some(col) = color {
+            self.colorize(col)
+        } else {
+            self
+        }
+    }
 }
