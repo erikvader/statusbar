0c7545a99e94f99f4e7b1ff75c06268757be1357
Erik Rimskog <erik.rimskog@gmail.com>
2021-06-07 23:28:16 +0200

    timer on TimeGen

--- src/tasks/generator/timegen.rs
+++ src/tasks/generator/timegen.rs
@@ -1,12 +1,24 @@
 use async_trait::async_trait;
 use chrono::prelude::*;
+use std::time::Instant;
 use super::{TimerGenerator,GenArg,Result};
 
-pub struct TimeGen{datetime: DateTime<Local>}
+struct Timer {
+    start: Instant,
+    now: Instant,
+}
+
+pub struct TimeGen {
+    datetime: DateTime<Local>,
+    timer: Option<Timer>,
+}
 
 impl TimeGen {
     pub fn new() -> Self {
-        TimeGen{datetime: Local::now()}
+        TimeGen{
+            datetime: Local::now(),
+            timer: None,
+        }
     }
 }
 
@@ -14,10 +26,13 @@ impl TimeGen {
 impl TimerGenerator for TimeGen {
     async fn update(&mut self) -> Result<()> {
         self.datetime = Local::now();
+        if let Some(t) = &mut self.timer {
+            t.now = Instant::now();
+        }
         Ok(())
     }
 
-    fn display(&self, _name: &str, arg: &GenArg) -> Result<String> {
+    fn display(&self, name: &str, arg: &GenArg) -> Result<String> {
         let d = match self.datetime.weekday() {
             Weekday::Mon => "Mån",
             Weekday::Tue => "Tis",
@@ -28,16 +43,45 @@ impl TimerGenerator for TimeGen {
             Weekday::Sun => "Sön",
         };
 
-        let s = arg.get_builder()
+        let mut s = arg.get_builder()
             .add(d)
             .add(" ")
-            .add(self.datetime.format("%Y-%m-%d %H:%M").to_string())
-            .to_string();
-        Ok(s)
+            .add(self.datetime.format("%Y-%m-%d").to_string())
+            .add(" ");
+
+        if let Some(t) = &self.timer {
+            let dur = (t.now - t.start).as_secs();
+            s = s.new_section()
+                .add(format!("{:02}:{:02}", dur / 60, dur % 60))
+                .colorize("green")
+                .everything();
+        } else {
+            s = s.add(self.datetime.format("%H:%M").to_string())
+        }
+
+        Ok(s.name_click(1, name)
+           .to_string())
+    }
+
+    async fn on_msg(&mut self, msg: String) -> Result<bool> {
+        if msg == "click 1" {
+            if self.timer.is_some() {
+                self.timer = None;
+            } else {
+                let n = Instant::now();
+                self.timer = Some(Timer {start: n, now: n});
+            }
+            return Ok(true);
+        }
+        Ok(false)
     }
 
     fn get_delay(&self, _arg: &GenArg) -> u64 {
-        // NOTE: the +1 is for safety
-        (60 - self.datetime.second() as u64) + 1
+        if self.timer.is_some() {
+            1
+        } else {
+            // NOTE: the +1 is for safety
+            (60 - self.datetime.second() as u64) + 1
+        }
     }
 }
